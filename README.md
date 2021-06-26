04b153bc21d3f7dfcd82ee18da48420d35fa3ded
Manassarn Manoonchai <manassarn@gmail.com>
2021-06-26 18:23:30 +0700

    :tada: Setup typescript project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kiimo
+
+Modify, and generate keyboard layout from single JSON file. Built with TypeScript.
---
+++ test/index.test.ts
@@ -0,0 +1,5 @@
+describe("It works", () => {
+  it("1+1 = 2", () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
