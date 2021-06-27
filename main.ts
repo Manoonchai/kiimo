e039e7688e41305081dda4b5b463ccbdca1d400c
Manassarn Manoonchai <manassarn@gmail.com>
2021-06-27 14:18:18 +0700

    :sparkles: Generate simple keylayout

--- main.ts
+++ main.ts
@@ -10,6 +10,7 @@ import {
   IsString,
   validate,
 } from "class-validator";
+import { js2xml } from "xml-js";
 
 export function generate() {
   fs.writeFileSync(path.join("output", "foo.keylayout"), "Hello!");
@@ -26,6 +27,222 @@ export async function validateLayout(
   }
 
   return !errors.length;
+}
+
+export async function generateLayout(content: Record<string, unknown>) {
+  const layout = plainToClass(Layout, content);
+  const errors = await validate(layout);
+
+  if (errors.length) {
+    throw new Error(errors.map((e) => e.toString()).join(", "));
+  }
+
+  const defaultKeyMapSelects = [
+    "anyControl? command?",
+    "anyShift anyControl? command?",
+    "anyOption anyControl? command?",
+    "anyShift anyOption anyControl? command?",
+    "caps anyControl? command?",
+    "caps anyShift anyControl? command?",
+    "caps anyOption anyControl? command?",
+    "caps anyShift anyOption anyControl? command?",
+  ];
+
+  const defaultKeys = [
+    { code: 0, output: "a" },
+    { code: 1, output: "s" },
+    { code: 2, output: "d" },
+    { code: 3, output: "f" },
+    { code: 4, output: "h" },
+    { code: 5, output: "g" },
+    { code: 6, output: "z" },
+    { code: 7, output: "x" },
+    { code: 8, output: "c" },
+    { code: 9, output: "v" },
+    { code: 10, output: "§" },
+    { code: 11, output: "b" },
+    { code: 12, output: "q" },
+    { code: 13, output: "w" },
+    { code: 14, output: "e" },
+    { code: 15, output: "r" },
+    { code: 16, output: "y" },
+    { code: 17, output: "t" },
+    { code: 18, output: "1" },
+    { code: 19, output: "2" },
+    { code: 20, output: "3" },
+    { code: 21, output: "4" },
+    { code: 22, output: "6" },
+    { code: 23, output: "5" },
+    { code: 24, output: "=" },
+    { code: 25, output: "9" },
+    { code: 26, output: "7" },
+    { code: 27, output: "-" },
+    { code: 28, output: "8" },
+    { code: 29, output: "0" },
+    { code: 30, output: "]" },
+    { code: 31, output: "o" },
+    { code: 32, output: "u" },
+    { code: 33, output: "[" },
+    { code: 34, output: "i" },
+    { code: 35, output: "p" },
+    { code: 36, output: "&#x000D;", unicode: true },
+    { code: 37, output: "l" },
+    { code: 38, output: "j" },
+    { code: 39, output: "&#x0027;", unicode: true },
+    { code: 40, output: "k" },
+    { code: 41, output: ";" },
+    { code: 42, output: "\\" },
+    { code: 43, output: "," },
+    { code: 44, output: "/" },
+    { code: 45, output: "n" },
+    { code: 46, output: "m" },
+    { code: 47, output: "." },
+    { code: 48, output: "&#x0009;", unicode: true },
+    { code: 49, output: " " },
+    { code: 50, output: "`" },
+    { code: 51, output: "&#x0008;", unicode: true },
+    { code: 52, output: "&#x0003;", unicode: true },
+    { code: 53, output: "&#x001B;", unicode: true },
+    { code: 65, output: "." },
+    { code: 66, output: "&#x001D;", unicode: true },
+    { code: 67, output: "*" },
+    { code: 69, output: "+" },
+    { code: 70, output: "&#x001C;", unicode: true },
+    { code: 71, output: "&#x001B;", unicode: true },
+    { code: 72, output: "&#x001F;", unicode: true },
+    { code: 75, output: "/" },
+    { code: 76, output: "&#x0003;", unicode: true },
+    { code: 77, output: "&#x001E;", unicode: true },
+    { code: 78, output: "-" },
+    { code: 81, output: "=" },
+    { code: 82, output: "0" },
+    { code: 83, output: "1" },
+    { code: 84, output: "2" },
+    { code: 85, output: "3" },
+    { code: 86, output: "4" },
+    { code: 87, output: "5" },
+    { code: 88, output: "6" },
+    { code: 89, output: "7" },
+    { code: 91, output: "8" },
+    { code: 92, output: "9" },
+    { code: 96, output: "&#x0010;", unicode: true },
+    { code: 97, output: "&#x0010;", unicode: true },
+    { code: 98, output: "&#x0010;", unicode: true },
+    { code: 99, output: "&#x0010;", unicode: true },
+    { code: 100, output: "&#x0010;", unicode: true },
+    { code: 101, output: "&#x0010;", unicode: true },
+    { code: 102, output: "&#x0010;", unicode: true },
+    { code: 103, output: "&#x0010;", unicode: true },
+    { code: 104, output: "&#x0010;", unicode: true },
+    { code: 105, output: "&#x0010;", unicode: true },
+    { code: 106, output: "&#x0010;", unicode: true },
+    { code: 107, output: "&#x0010;", unicode: true },
+    { code: 108, output: "&#x0010;", unicode: true },
+    { code: 109, output: "&#x0010;", unicode: true },
+    { code: 110, output: "&#x0010;", unicode: true },
+    { code: 111, output: "&#x0010;", unicode: true },
+    { code: 112, output: "&#x0010;", unicode: true },
+    { code: 113, output: "&#x0010;", unicode: true },
+    { code: 114, output: "&#x0005;", unicode: true },
+    { code: 115, output: "&#x0001;", unicode: true },
+    { code: 116, output: "&#x000B;", unicode: true },
+    { code: 117, output: "&#x007F;", unicode: true },
+    { code: 118, output: "&#x0010;", unicode: true },
+    { code: 119, output: "&#x0004;", unicode: true },
+    { code: 120, output: "&#x0010;", unicode: true },
+    { code: 121, output: "&#x000C;", unicode: true },
+    { code: 122, output: "&#x0010;", unicode: true },
+    { code: 123, output: "&#x001C;", unicode: true },
+    { code: 124, output: "&#x001D;", unicode: true },
+    { code: 125, output: "&#x001F;", unicode: true },
+    { code: 126, output: "&#x001E;", unicode: true },
+  ];
+
+  const document = {
+    declaration: { attributes: { version: "1.1", encoding: "UTF-8" } },
+    elements: [
+      {
+        doctype:
+          'keyboard SYSTEM "file://localhost/System/Library/DTDs/KeyboardLayout.dtd"',
+        type: "doctype",
+      },
+      {
+        type: "element",
+        name: "keyboard",
+        attributes: {
+          group: "0",
+          id: "5991",
+          name: layout.name,
+          maxout: "1",
+        },
+        elements: [
+          {
+            type: "element",
+            name: "layouts",
+            elements: [
+              {
+                type: "element",
+                name: "layout",
+                attributes: {
+                  first: "0",
+                  last: "0",
+                  mapSet: "defaultKeyMapSet",
+                  modifiers: "defaultModifierMap",
+                },
+              },
+            ],
+          },
+          {
+            type: "element",
+            name: "modifierMap",
+            attributes: { id: "defaultModifierMap", defaultIndex: "0" },
+            elements: defaultKeyMapSelects.map((keyMapSelect, idx) => ({
+              type: "element",
+              name: "keyMapSelect",
+              attributes: {
+                mapIndex: `${idx}`,
+              },
+              elements: [
+                {
+                  type: "element",
+                  name: "modifier",
+                  attributes: {
+                    keys: keyMapSelect,
+                  },
+                },
+              ],
+            })),
+          },
+          {
+            type: "element",
+            name: "keyMapSet",
+            attributes: { id: "defaultKeyMapSet", defaultIndex: "0" },
+            elements: [
+              {
+                type: "element",
+                name: "keyMap",
+                attributes: { index: "0" },
+                elements: defaultKeys.map(({ code, output, unicode }) => {
+                  let overrideKey = layout.keys[output]?.[0] || output;
+
+                  if (unicode) {
+                    overrideKey = encodeURIComponent(output);
+                  }
+                  return {
+                    type: "element",
+                    name: "key",
+                    attributes: { code, output: overrideKey },
+                  };
+                }),
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+
+  return js2xml(document, { spaces: 2 });
 }
 
 export class Layout {
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,7 +1,8 @@
 import fs from "fs";
 import path from "path";
 import { js2xml, xml2js } from "xml-js";
-import { generate, validateLayout } from "../main";
+import { generate, generateLayout, validateLayout } from "../main";
+import replace from "replace-in-file";
 
 describe("#generate", () => {
   beforeAll(() => {
@@ -20,16 +21,15 @@ describe("#generate", () => {
       "utf8"
     );
     const jsContent = xml2js(content);
-    // console.dir(jsContent);
-    const xmlContent = js2xml(jsContent, {
+
+    js2xml(jsContent, {
       compact: false,
       spaces: 2,
     });
-    console.log({ xmlContent: xmlContent.substring(0, 10) });
   });
 });
 
-describe("json input", () => {
+describe("validateLayout", () => {
   it("passes structure validation", async () => {
     const validJson = JSON.parse(
       fs.readFileSync(
@@ -56,5 +56,266 @@ describe("json input", () => {
     invalidJson["keys"] = { a: ["a", "A"] };
     invalidJson["layers"][0] = "InvalidLayerName";
     expect(await validateLayout(invalidJson)).toEqual(false);
+  });
+});
+
+describe("generateLayout", () => {
+  it("receives layout json and returns keylayout xml correctly", async () => {
+    const manoonchaiJson = JSON.parse(
+      fs.readFileSync(
+        path.join(process.cwd(), "input", "manoonchai.json"),
+        "utf8"
+      )
+    );
+
+    const manoonchaiXml = await generateLayout(manoonchaiJson);
+
+    expect(manoonchaiXml).toBeDefined();
+
+    const manoonchai = xml2js(manoonchaiXml);
+
+    expect(manoonchai).not.toStrictEqual({});
+
+    expect(manoonchai).toEqual(
+      expect.objectContaining({
+        declaration: { attributes: { version: "1.1", encoding: "UTF-8" } },
+      })
+    );
+
+    fs.writeFileSync("output/tmp.xml", js2xml(manoonchai, { spaces: 2 }), {});
+
+    const options = {
+      files: "output/tmp.xml",
+      from: /%26%23x([0-9A-F]+)%3B/g,
+      to: "&#x$1;",
+    };
+
+    try {
+      const results = replace.sync(options);
+      console.log("Replacement results:", results);
+    } catch (error) {
+      console.error("Error occurred:", error);
+    }
+
+    expect(manoonchai.elements[0]).toEqual({
+      doctype:
+        'keyboard SYSTEM "file://localhost/System/Library/DTDs/KeyboardLayout.dtd"',
+      type: "doctype",
+    });
+
+    // <keyboard group="0" id="5991" name="Thai - Manoonchai v0.3" maxout="1">
+    const keyboard = manoonchai.elements[1];
+    expect(keyboard).toEqual(
+      expect.objectContaining({
+        type: "element",
+        name: "keyboard",
+        attributes: {
+          group: "0",
+          id: "5991",
+          name: "Manoonchai",
+          maxout: "1",
+        },
+      })
+    );
+
+    // <layouts>
+    //     <layout first="0" last="0" mapSet="a8" modifiers="30"/>
+    // </layouts>
+    const layouts = keyboard.elements[0];
+    expect(layouts).toEqual({
+      type: "element",
+      name: "layouts",
+      elements: [
+        {
+          type: "element",
+          name: "layout",
+          attributes: {
+            first: "0",
+            last: "0",
+            modifiers: "defaultModifierMap",
+            mapSet: "defaultKeyMapSet",
+          },
+        },
+      ],
+    });
+
+    // <modifierMap id="defaultModifierMap" defaultIndex="0">
+    //   <keyMapSelect mapIndex="0">
+    //     <modifier keys="anyControl? command?" />
+    //   </keyMapSelect>
+    //   ...
+    // </modifierMap>
+    const modifierMap = keyboard.elements[1];
+    expect(modifierMap).toEqual({
+      type: "element",
+      name: "modifierMap",
+      attributes: { id: "defaultModifierMap", defaultIndex: "0" },
+      elements: [
+        {
+          type: "element",
+          name: "keyMapSelect",
+          attributes: {
+            mapIndex: "0",
+          },
+          elements: [
+            {
+              type: "element",
+              name: "modifier",
+              attributes: {
+                keys: "anyControl? command?",
+              },
+            },
+          ],
+        },
+        {
+          type: "element",
+          name: "keyMapSelect",
+          attributes: {
+            mapIndex: "1",
+          },
+          elements: [
+            {
+              type: "element",
+              name: "modifier",
+              attributes: {
+                keys: "anyShift anyControl? command?",
+              },
+            },
+          ],
+        },
+        {
+          type: "element",
+          name: "keyMapSelect",
+          attributes: {
+            mapIndex: "2",
+          },
+          elements: [
+            {
+              type: "element",
+              name: "modifier",
+              attributes: {
+                keys: "anyOption anyControl? command?",
+              },
+            },
+          ],
+        },
+        {
+          type: "element",
+          name: "keyMapSelect",
+          attributes: {
+            mapIndex: "3",
+          },
+          elements: [
+            {
+              type: "element",
+              name: "modifier",
+              attributes: {
+                keys: "anyShift anyOption anyControl? command?",
+              },
+            },
+          ],
+        },
+        {
+          type: "element",
+          name: "keyMapSelect",
+          attributes: {
+            mapIndex: "4",
+          },
+          elements: [
+            {
+              type: "element",
+              name: "modifier",
+              attributes: {
+                keys: "caps anyControl? command?",
+              },
+            },
+          ],
+        },
+        {
+          type: "element",
+          name: "keyMapSelect",
+          attributes: {
+            mapIndex: "5",
+          },
+          elements: [
+            {
+              type: "element",
+              name: "modifier",
+              attributes: {
+                keys: "caps anyShift anyControl? command?",
+              },
+            },
+          ],
+        },
+        {
+          type: "element",
+          name: "keyMapSelect",
+          attributes: {
+            mapIndex: "6",
+          },
+          elements: [
+            {
+              type: "element",
+              name: "modifier",
+              attributes: {
+                keys: "caps anyOption anyControl? command?",
+              },
+            },
+          ],
+        },
+        {
+          type: "element",
+          name: "keyMapSelect",
+          attributes: {
+            mapIndex: "7",
+          },
+          elements: [
+            {
+              type: "element",
+              name: "modifier",
+              attributes: {
+                keys: "caps anyShift anyOption anyControl? command?",
+              },
+            },
+          ],
+        },
+      ],
+    });
+
+    // <keyMapSet id="defaultKeyMapSet">
+    //   <keyMap index="0">
+    //     <key code="10" output="`" />
+    const expectedKeys = [
+      { code: "18", output: "1" },
+      { code: "19", output: "2" },
+      { code: "20", output: "3" },
+      { code: "21", output: "4" },
+      { code: "23", output: "5" },
+      { code: "22", output: "6" },
+      { code: "26", output: "7" },
+      { code: "28", output: "8" },
+      { code: "25", output: "9" },
+      { code: "29", output: "0" },
+    ];
+    const keyMapSet = keyboard.elements[2];
+    expect(keyMapSet).toEqual({
+      type: "element",
+      name: "keyMapSet",
+      attributes: { id: "defaultKeyMapSet", defaultIndex: "0" },
+      elements: [
+        {
+          type: "element",
+          name: "keyMap",
+          attributes: { index: "0" },
+          elements: expect.arrayContaining(
+            expectedKeys.map(({ code, output }) => ({
+              type: "element",
+              name: "key",
+              attributes: { code, output },
+            }))
+          ),
+        },
+      ],
+    });
   });
 });
